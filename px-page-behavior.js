677dc55f1b1c540de32cba2757e870bd5c9306ab
Jonnie Spratley <jonnie.spratley@ge.com>
2016-05-05 09:57:56 -0700

    fix: updated scrolling issue

--- px-page-behavior.js
+++ px-page-behavior.js
@@ -1,20 +1,23 @@
 var PageBehavior = {
-  ready: function () {
+  ready: function() {
     if (this.dialog) {
       this.toggleClass('dialog');
     }
     this.fire('page:ready', this);
   },
-  attached: function () {
-
-
-
+  attached: function() {
     if (this.parentNode && this.parentNode.localName === 'px-pages') {
       this.viewContainer = this.parentNode;
     }
+
+
     this._fixNavbar();
   },
-  _fixNavbar: function () {
+  _fixContent: function() {
+    var contentHeight = this.$.pageContent.offsetHeight;
+    console.warn('Resize content to', contentHeight);
+  },
+  _fixNavbar: function() {
     //var w = this.parentNode.offsetWidth;
     //var h = this.parentNode.offsetHeight;
     var pageContent = this.$.pageContent;
@@ -31,54 +34,54 @@ var PageBehavior = {
       this.navbar = pageNavbar;
     }
   },
-  show: function () {
+  show: function() {
     console.log('INFO', 'show view', this.id);
     this.toggleClass('current', false, this);
   },
-  hide: function () {
+  hide: function() {
     console.log('INFO', 'hide view', this.id);
     this.toggleClass('hidden', true, this);
   },
-  update: function () {
+  update: function() {
     console.log('INFO', 'update view', this.id);
   },
-  currentView: function () {
+  currentView: function() {
     console.log('INFO', 'current view', this.id);
     this.child()[0].toggleClass('current', true, this);
   },
-  nextView: function () {
+  nextView: function() {
     console.log('INFO', 'next view', this.id);
     this.toggleClass('next', true, this);
   },
-  previousView: function () {
+  previousView: function() {
     console.log('INFO', 'previous view', this.id);
     this.toggleClass('previous', true, this);
   },
-  contextChanged: function (newContext, oldContext) {
+  contextChanged: function(newContext, oldContext) {
     console.log('contextChanged', newContext, oldContext);
   },
 
   //I handle loading a page from a url
-  _tmplChanged: function (newVal, oldVal) {
+  _tmplChanged: function(newVal, oldVal) {
     var _this = this,
       html = '';
     if (newVal) {
       console.log(this.id, 'Load remote html', newVal);
-      this.importHref(newVal, function (e) {
+      this.importHref(newVal, function(e) {
         html = e.target.import.body.innerHTML;
         _this.html(html);
-      }, function (err) {
+      }, function(err) {
         console.error('Error loading page', err);
       });
     }
   },
-  open: function () {
+  open: function() {
     if (this.dialog) {
       this.toggleClass('et-page-current');
       this.toggleClass('is-open');
     }
   },
-  close: function () {
+  close: function() {
     if (this.dialog) {
       this.toggleClass('et-page-current');
       this.toggleClass('is-open');
